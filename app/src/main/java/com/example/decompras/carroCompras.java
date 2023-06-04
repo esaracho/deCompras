4f93dc9bd3a4e23ee47b31e04d0532774a142a89
chino <sarachoesteban@gmail.com>
2023-06-03 22:30:15 -0300

    Commit inicial del proyecto con clases y layouts

---
+++ app/src/main/java/com/example/decompras/carroCompras.java
@@ -0,0 +1,15 @@
+package com.example.decompras;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class carroCompras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.carro_compras);
+    }
+
+}
---
+++ app/src/main/java/com/example/decompras/quienesSomos.java
@@ -0,0 +1,15 @@
+package com.example.decompras;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class quienesSomos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quienes_somos);
+    }
+
+}
