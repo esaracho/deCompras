b7835a1847eedb1f4ae60d1732624548b2ef9e0b
flavi <ucpflavio@gmail.com>
2023-06-19 11:39:57 -0300

    Modificacion de video,video , descripcion ,boton volver,

--- app/src/main/java/com/example/decompras/quienesSomos.java
+++ app/src/main/java/com/example/decompras/quienesSomos.java
@@ -1,43 +1,124 @@
 package com.example.decompras;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+import android.widget.Button;
+
 public class quienesSomos extends AppCompatActivity {
 
+    private static final String VIDEO_SAMPLE = "decompras.mp4";
+    private VideoView mVideoView;
+    private int mPosiciónActual = 0;
+
+    private TextView mBufferingTextView;
+    private static final String PLAYBACK_TIME = "play_time";
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quienes_somos);
-
-
-        Button backButton = findViewById(R.id.backButton);
-
-
-        backButton.setOnClickListener(v -> {
-
-            Intent intent = new Intent(quienesSomos.this, MainActivity.class);
-            startActivity(intent);
+        Button btnVolverMain = findViewById(R.id.btnVolverMain);
+        btnVolverMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Volver al MainActivity
+                onBackPressed();
+            }
         });
 
+        mBufferingTextView = findViewById(R.id.buffering_textview);
+        mVideoView = findViewById(R.id.videoview);
 
-        VideoView videoView = findViewById(R.id.videoView);
+        if (savedInstanceState != null) {
+            mPosiciónActual = savedInstanceState.getInt(PLAYBACK_TIME);
+        }
+        Log.d(LOG_TAG, "¡Botón presionado!" + mPosiciónActual);
 
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        videoView.setMediaController(mediaController);
-
-        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.videopruebas;
-
-
-        videoView.setVideoURI(Uri.parse(videoPath));
-        videoView.start();
+        MediaController controlador = new MediaController(this);
+        controlador.setMediaPlayer(mVideoView);
+        mVideoView.setMediaController(controlador);
     }
-}
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        inicializarPlayer();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releasePlayer();
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(PLAYBACK_TIME, mPosiciónActual);
+        super.onSaveInstanceState(outState);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mPosiciónActual = mVideoView.getCurrentPosition();
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void inicializarPlayer() {
+        mBufferingTextView.setVisibility(VideoView.VISIBLE);
+        Uri videoUri = getMedia(VIDEO_SAMPLE);
+        mVideoView.setVideoURI(videoUri);
+
+        mVideoView.setOnPreparedListener(
+                new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+                        mBufferingTextView.setVisibility(VideoView.INVISIBLE);
+                        if (mPosiciónActual > 0) {
+                            mVideoView.seekTo(mPosiciónActual);
+                        } else {
+
+                            mVideoView.seekTo(1);
+                        }
+                        mVideoView.start();
+                    }
+                });
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Toast.makeText(quienesSomos.this, "Reproducción completa", Toast.LENGTH_SHORT).show();
+                mVideoView.seekTo(1);
+            }
+        });
+    }
+
+    private void releasePlayer() {
+        mVideoView.stopPlayback();
+    }
+
+    private Uri getMedia(String mediaName) {
+        if (URLUtil.isValidUrl(mediaName)) {
+
+            return Uri.parse(mediaName);
+        } else {
+
+            return Uri.parse("android.resource://" + getPackageName() + "/"+ R.raw.decompras);
+        }
+    }
+
+
+}
